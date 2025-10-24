02c3b0867187b1caa07cbc253188df601d978c38
Abdul Moeed <147938340+abdulmoeed1090@users.noreply.github.com>
2025-10-24 20:12:15 +0500

    Add modulus function to calculator

--- calculator.js
+++ calculator.js
@@ -10,6 +10,9 @@ function multiply(a, b) {
     return a * b;
 }
 
+function modulus(a, b) {
+    return a % b;
+}
 function divide(a, b) {
     if (b === 0) throw new Error("Cannot divide by zero");
     return a / b;
